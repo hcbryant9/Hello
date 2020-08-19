9f2c4ab2b88bfdcd2299a827cd9343f4fd4e56d3
hcbryant9 <69921401+hcbryant9@users.noreply.github.com>
2020-08-19 14:53:51 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/**********************************************************
+* Java Program Name
+***********************************************************
+* Henry Bryant
+* 8/18/20
+* Intro to Programming - 004
+**********************************************************/
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+	System.out.println("<Henry Bryant>");
+	System.out.println("<Lab 1 Project>");
+	System.out.println("<Introduction to the Course>");
+	System.out.println("<8/19/2020>");
+	System.out.println("<Intro to Programming - 004>");
+	System.out.println();
+	}
+}
